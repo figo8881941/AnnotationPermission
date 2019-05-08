645189d07345c0fe13b79fabb5955220aa49ad39
figo1874 <176511609@qq.com>
2019-05-08 17:45:42 +0800

    Add init content provider

--- library/src/main/java/com/duoduo/annotationpermission/library/aspectj/PermissionCheckAspectJ.java
+++ library/src/main/java/com/duoduo/annotationpermission/library/aspectj/PermissionCheckAspectJ.java
@@ -1,17 +1,15 @@
 package com.duoduo.annotationpermission.library.aspectj;
 
-import android.app.Fragment;
 import android.content.Context;
-import android.view.View;
 
 import com.duoduo.annotationpermission.library.ICheckAndRequestPermissionListener;
-import com.duoduo.annotationpermission.library.IContextHolder;
 import com.duoduo.annotationpermission.library.PermissionUtils;
 import com.duoduo.annotationpermission.library.annotation.NeedPermission;
 import com.duoduo.annotationpermission.library.annotation.OnAlwaysDeniedPermission;
 import com.duoduo.annotationpermission.library.annotation.OnDeniedPermission;
 import com.duoduo.annotationpermission.library.annotation.OnGrantedPermission;
 import com.duoduo.annotationpermission.library.annotation.OnShowRationable;
+import com.duoduo.annotationpermission.library.context.ContextFinder;
 import com.duoduo.annotationpermission.library.denied.AlwaysDeniedExecutor;
 import com.duoduo.annotationpermission.library.entity.AlwaysDeniedPermissionEntity;
 import com.duoduo.annotationpermission.library.entity.DeniedPermissionEntity;
@@ -26,7 +24,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -41,7 +38,6 @@ public class PermissionCheckAspectJ {
     private static HashMap<Class, Method> classDeniedPermissionMethodCache = new HashMap<Class, Method>();
     private static HashMap<Class, Method> classAlwaysDeniedPermissionMethodCache = new HashMap<Class, Method>();
     private static HashMap<Class, Method> classShowRationableMethodCache = new HashMap<Class, Method>();
-    private static HashMap<Class, Field> classContextFieldCache = new HashMap<Class, Field>();
     private static HashSet<Method> onceMethodRecord = new HashSet<Method>();
 
     private final String TAG = "PermissionCheckAspectJ";
@@ -54,7 +50,7 @@ public class PermissionCheckAspectJ {
     public void checkPermission(final ProceedingJoinPoint joinPoint) throws Throwable {
 
         //尝试获取Context
-        final Context context = tryToGetContext(joinPoint);
+        final Context context = ContextFinder.findContext(joinPoint);
 
         //获取注解参数
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
@@ -364,78 +360,5 @@ public class PermissionCheckAspectJ {
             }
         }
         return method;
-    }
-
-    /**
-     * 尝试获取Context的方法
-     *
-     * @param joinPoint
-     * @return
-     */
-    private Context tryToGetContext(ProceedingJoinPoint joinPoint) {
-        Context context = null;
-        try {
-            Object targetObject = joinPoint.getTarget();
-            if (targetObject instanceof Context) {
-                context = (Context) targetObject;
-            } else if (targetObject instanceof Fragment) {
-                context = ((Fragment) targetObject).getActivity();
-            } else if (targetObject instanceof android.support.v4.app.Fragment) {
-                context = ((android.support.v4.app.Fragment) targetObject).getActivity();
-            } else if (targetObject instanceof View) {
-                context = ((View) targetObject).getContext();
-            } else if (targetObject instanceof IContextHolder) {
-                context = ((IContextHolder) targetObject).getContext();
-            } else {
-                //如果上面的类型都不是
-                //尝试通过反射，获取targetObject的Context字段
-                //如果没有Context字段，则获取失败
-                Class targetObjectClass = targetObject.getClass();
-                Field field = getClassContextField(targetObjectClass);
-                if (field != null) {
-                    field.setAccessible(true);
-                    context = (Context) field.get(targetObject);
-                }
-
-            }
-            if (context != null) {
-                context = context.getApplicationContext();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return context;
-    }
-
-    /**
-     * 获取类Context字段
-     *
-     * @param targetClass
-     */
-    private Field getClassContextField(Class targetClass) {
-        Field field = null;
-        synchronized (classContextFieldCache) {
-            field = classContextFieldCache.get(targetClass);
-            if (field == null) {
-                for (Class forclass = targetClass; !forclass.equals(Object.class) && field == null; forclass = forclass.getSuperclass()) {
-                    Field[] fields = forclass.getDeclaredFields();
-                    if (fields != null) {
-                        int size = fields.length;
-                        for (int i = 0; i < size; i++) {
-                            Field fieldItem = fields[i];
-                            Class fieldType = fieldItem.getType();
-                            if (fieldType.equals(Context.class)) {
-                                field = fieldItem;
-                                break;
-                            }
-                        }
-                    }
-                }
-                if (field != null) {
-                    classContextFieldCache.put(targetClass, field);
-                }
-            }
-        }
-        return field;
     }
 }
---
+++ library/src/main/java/com/duoduo/annotationpermission/library/context/ContextFinder.java
@@ -0,0 +1,103 @@
+package com.duoduo.annotationpermission.library.context;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.view.View;
+
+import com.duoduo.annotationpermission.library.IContextHolder;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
+/**
+ * Context获取类
+ */
+public class ContextFinder {
+
+    private static HashMap<Class, Field> classContextFieldCache = new HashMap<Class, Field>();
+
+    private static Context sContext;
+
+    public static Context findContext(ProceedingJoinPoint joinPoint) {
+        if (sContext != null) {
+            return sContext;
+        }
+        synchronized (ContextFinder.class) {
+            if (sContext != null) {
+                return sContext;
+            }
+            Context context = null;
+            try {
+                Object targetObject = joinPoint.getTarget();
+                if (targetObject instanceof Context) {
+                    context = (Context) targetObject;
+                } else if (targetObject instanceof Fragment) {
+                    context = ((Fragment) targetObject).getActivity();
+                } else if (targetObject instanceof android.support.v4.app.Fragment) {
+                    context = ((android.support.v4.app.Fragment) targetObject).getActivity();
+                } else if (targetObject instanceof View) {
+                    context = ((View) targetObject).getContext();
+                } else if (targetObject instanceof IContextHolder) {
+                    context = ((IContextHolder) targetObject).getContext();
+                } else {
+                    //如果上面的类型都不是
+                    //尝试通过反射，获取targetObject的Context字段
+                    //如果没有Context字段，则获取失败
+                    Class targetObjectClass = targetObject.getClass();
+                    Field field = getClassContextField(targetObjectClass);
+                    if (field != null) {
+                        field.setAccessible(true);
+                        context = (Context) field.get(targetObject);
+                    }
+
+                }
+                if (context != null) {
+                    sContext = context.getApplicationContext();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sContext;
+    }
+
+    public static void setContext(Context context) {
+        synchronized (ContextFinder.class) {
+            sContext = context;
+        }
+    }
+
+    /**
+     * 获取类Context字段
+     *
+     * @param targetClass
+     */
+    private static Field getClassContextField(Class targetClass) {
+        Field field = null;
+        synchronized (classContextFieldCache) {
+            field = classContextFieldCache.get(targetClass);
+            if (field == null) {
+                for (Class forclass = targetClass; !forclass.equals(Object.class) && field == null; forclass = forclass.getSuperclass()) {
+                    Field[] fields = forclass.getDeclaredFields();
+                    if (fields != null) {
+                        int size = fields.length;
+                        for (int i = 0; i < size; i++) {
+                            Field fieldItem = fields[i];
+                            Class fieldType = fieldItem.getType();
+                            if (fieldType.equals(Context.class)) {
+                                field = fieldItem;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (field != null) {
+                    classContextFieldCache.put(targetClass, field);
+                }
+            }
+        }
+        return field;
+    }
+}
